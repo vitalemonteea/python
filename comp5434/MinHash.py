75616e96242105aa8c4c701a77df11f211e087bf
“Vitalemonteea” <2192189495@qq.com>
2024-10-08 01:30:12 +0800

    commit minHash

--- comp5434/MinHash.py
+++ comp5434/MinHash.py
@@ -15,7 +15,46 @@ def get_matrix():
 
 def get_minHashMatrix(matrix):
     #step2 generate the minHash signature matrix
-    minHashMatrix=np.zeros((7,4))
+    num_rows,num_cols=matrix.shape
+    num_hash_functions=3  
+    # define the hash functions
+    def h1(x): return (2*x + 1) % 7
+    def h2(x): return (3*x + 2) % 7
+    def h3(x): return (4*x + 3) % 7
+
+    hash_functions=[h1,h2,h3]
+
+    minHashMatrix = np.full((num_hash_functions,num_cols),np.inf)
+
+    for i in range(num_rows):
+        for j in range(num_cols):
+            if matrix[i,j]==1:
+                for k,hash_func in enumerate(hash_functions):
+                    minHashMatrix[k,j]=min(minHashMatrix[k,j],hash_func(i)) 
     #Make hash changes
     return minHashMatrix
+
+#calculate the similarity
+def calculate_similarity_matrix(minHashMatrix):
+    num_hash,col=minHashMatrix.shape
+    similarity_matrix=np.zeros((col,col))
+
+    for i in range(col):
+        for j in range(i+1,col):
+            similarity_matrix[i,j]=np.sum(minHashMatrix[:,i]==minHashMatrix[:,j])/num_hash
+            similarity_matrix[j,i]=similarity_matrix[i,j]
+
+    return similarity_matrix
+
+
+def main():
+   matrix=get_matrix()
+   minHashMatrix=get_minHashMatrix(matrix)
+   similarity_matrix=calculate_similarity_matrix(minHashMatrix)
+   print(similarity_matrix)
+
+if __name__ == "__main__":
+    main()
+
+
 
